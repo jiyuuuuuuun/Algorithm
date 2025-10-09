cce1348c577d3b4e12a62a277a8ecedff886a194
JIYUN <167059023+jiyuuuuuuun@users.noreply.github.com>
2025-10-09 18:12:43 +0900

    Update 바이러스.py
    dfs(재귀, 스택), bfs 각각의 방법으로 푸는 방법 추가

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,25 +1,94 @@
-# 2606 바이러스
-import sys
-input = sys.stdin.readline
+# 탐색 알고리즘: DFS/BFS
+#### BFS
 
-visited = set()
+from collections import deque
 
-def DFS(cur):
-  visited.add(cur)
+N = int(input()) # 컴퓨터의 수
+M = int(input()) # 네트워크 상에서 연결되어있는 컴퓨터의 번호 쌍 수
 
-  for nxt in adj[cur]: #다음에 찾아볼 노드를 현재리스트에서 하나씩 뽑아놓는다.
-    if nxt not in visited:
-      DFS(nxt)
-      
-
-N = int(input())
-M = int(input())
-adj = [[] for _ in range(N+1)]
-
+network = [[] for _ in range(N+1)]
 for _ in range(M):
   a, b = map(int, input().split())
-  adj[a].append(b)
-  adj[b].append(a) #a와 b가 연결되어있음을 알리는 코드 넣어준다.
+  network[a].append(b)
+  network[b].append(a)
 
+queue = deque([1]) # 1부터 시작
+visited = [0] * (N+1)
+visited[1] = 1 # 시작은 방문처리
+cnt = 0
+
+while queue:
+  current = queue.popleft()
+  
+  for connect in network[current]:
+    if visited[connect] == 0: # 방문 안했다면
+      visited[connect] = 1 # 방문처리
+      cnt += 1
+      queue.append(connect) # 다음탐색대상으로 지정
+      
+print(cnt)
+
+
+
+#### DFS(재귀)
+
+# 탐색 알고리즘: DFS/BFS
+
+
+N = int(input()) # 컴퓨터의 수
+M = int(input()) # 네트워크 상에서 연결되어있는 컴퓨터의 번호 쌍 수
+
+network = [[] for _ in range(N+1)]
+for _ in range(M):
+  a, b = map(int, input().split())
+  network[a].append(b)
+  network[b].append(a)
+
+visited = [0] * (N+1)
+cnt = 0
+
+def DFS(current):
+  global cnt
+  visited[current] = 1
+  
+  for connect in network[current]:
+    if visited[connect] == 0: # 방문 안했다면
+      cnt += 1
+      DFS(connect) # 해당 노드로 이동(재귀호츌)
+      
 DFS(1)
-print(len(visited)-1)
+print(cnt)
+
+
+#### DFS(스택)
+
+# 탐색 알고리즘: DFS/BFS
+
+
+N = int(input()) # 컴퓨터의 수
+M = int(input()) # 네트워크 상에서 연결되어있는 컴퓨터의 번호 쌍 수
+
+network = [[] for _ in range(N+1)]
+for _ in range(M):
+  a, b = map(int, input().split())
+  network[a].append(b)
+  network[b].append(a)
+
+stack = [1] # 1부터 시작
+visited = [0] * (N+1)
+cnt = 0
+
+while stack:
+  current = stack.pop()
+  
+  if visited[current] == 0: # 방문 안했다면
+    visited[current] = 1 # 방문처리
+    
+    if current != 1:
+      cnt += 1
+
+    for connect in network[current]:
+      if not visited[connect]:
+        stack.append(connect) # 다음 탐색 대상으로 지정
+      
+print(cnt)
